acfc986542b38b864346e2bd8e66b51bb3ca91b5
emilianoagu <emilianoagu@hotmail.com>
2022-04-17 18:29:11 -0300

    Agregue el crear la lista cin los elementos li de un ul que son las cards de los estilos a elegir

--- JS/index.js
+++ JS/index.js
@@ -17,12 +17,21 @@ const listaAmbientes = [
     { id: 03, ambiente: 'Comedor' }, { id: 04, ambiente: 'Estar' },
     { id: 05, ambiente: 'Dormitorio' }, { id: 06, ambiente: 'Quincho' }, { id: 07, ambiente: 'Cochera' }, { id: 08, ambiente: 'Otro' }
 ]
+
+const listaEstilos = [
+    {id:01, estilo: 'Clasico'}, {id:02, estilo: 'Vintage'}, {id:03, estilo: 'Green'}, {id:04, estilo: 'Rústico'}, {id:05, estilo: 'Moderno'}]
+
 const agregaAmbienteBtn = document.querySelector('#agregarAmb-Btn')
 const selectAmbientes = document.querySelector('#ambientes-Select')
 const anchoInput = document.querySelector('#ancho-Input')
 const largoInput = document.querySelector('#largo-Input')
 const divAmbientes = document.querySelector('#div-ambientes')
+const ulEstilos = document.querySelector('#ul-estilos')
 let listaAmbientesAgreados = []
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    mostrarEstilo(listaEstilos)
+})
 
 // CREO Y CARGO LOS OPTIONS DEL SELECT-------------------
 listaAmbientes.forEach(ambienteLista => {
@@ -60,7 +69,7 @@ agregaAmbienteBtn.addEventListener('click', function () {
 //     console.log(listaAgregar);
 //     ulElement.innerHTML = listaAgregar
 // }
-function cargaEstilos(objeto) {
+function cargaStyles(objeto) {
     objeto.style.display = 'inline-block'
     objeto.style.padding = '8px 30px 4px 30px'
     objeto.style.width = '100px'
@@ -80,23 +89,23 @@ function mostrar(listaAmb) {
         const nombreAmb = document.createElement('p')
         nombreAmb.classList.add('valores')
         nombreAmb.textContent = amb.nombre
-        cargaEstilos(nombreAmb)
+        cargaStyles(nombreAmb)
 
 
         const anchoAmb = document.createElement('p')
         anchoAmb.classList.add('valores')
         anchoAmb.textContent = amb.ancho
-        cargaEstilos(anchoAmb)
+        cargaStyles(anchoAmb)
 
         const largoAmb = document.createElement('p')
         largoAmb.classList.add('valores')
         largoAmb.textContent = amb.largo
-        cargaEstilos(largoAmb)
+        cargaStyles(largoAmb)
 
         const superficieAmb = document.createElement('p')
         superficieAmb.classList.add('valores')
         superficieAmb.textContent = amb.superficie
-        cargaEstilos(superficieAmb)
+        cargaStyles(superficieAmb)
 
         const iconoBorrar = document.createElement('img')
         iconoBorrar.src = '/multimedia/img/trash-2.svg'
@@ -131,4 +140,26 @@ function borrarAmbiente(id){
     console.log(listaAmbientesAgreados.indexOf(listaAmbientesAgreados.find(item => item.id === id)))
     listaAmbientesAgreados.splice(listaAmbientesAgreados.indexOf(listaAmbientesAgreados.find(item => item.id === id)),1) 
     mostrar(listaAmbientesAgreados)
+}
+
+
+function mostrarEstilo(estilo) {
+    ulEstilos.innerHTML = ''
+    estilo.forEach(est => {
+
+        const liEstilo = document.createElement('li')
+        liEstilo.classList.add('img'+est.id)
+
+        const estiloAmb = document.createElement('h3')
+        estiloAmb.textContent = est.estilo
+        
+
+        const inputAmb = document.createElement('input')
+        inputAmb.id = 'check'+est.estilo
+        inputAmb.type = 'checkbox'
+        
+        estiloAmb.appendChild(inputAmb)
+        liEstilo.appendChild(estiloAmb)
+        ulEstilos.appendChild(liEstilo)
+    })
 }
